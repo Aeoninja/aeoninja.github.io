dcbcce92c5ff876e17138b53b543503347b65d67
VMK <55680799+welitin14@users.noreply.github.com>
2019-10-28 10:40:29 +0200

    javaaaaa

--- tuoli.js
+++ tuoli.js
@@ -1,26 +1,56 @@
-//Checks if element is visible
-function isScrolledIntoView(el) {
-    var rect = el.getBoundingClientRect();
-    var elemTop = rect.top;
-    var elemBottom = rect.bottom;
+function ShowPosition()
+{
+if (navigator.geolocation)
+navigator.geolocation.getCurrentPosition(DisplayResult, DisplayError)
+else
+alert("Browser does not support geolocation");
+}
+function DisplayResult(Position)
+{
+/*var message = " Latitude: " + Position.coords.latitude;
+message += " Longitude: " + Position.coords.longitude;
+message += " Accuracy: " + Position.coords.accuracy + " meters ";
+alert(message);*/
 
-    // Only completely visible elements return true:
-    //var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    // Partially visible elements return true:
-    var isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    return isVisible;
+var context = document.getElementById('mapCanvas').getContext("2d");
+	
+var img = new Image();
+img.onload = function () {
+    context.drawImage(img, 0, 0);
 }
-//Loops through all elements, fading each in or out based on visibility
-function fadeOnView()  {
-	document.querySelectorAll('body *').forEach(function(node){
-		if (isScrolledIntoView(node)){
-			$(node).fadeIn(5000);
-		}
-		else if (!isScrolledIntoView(node)){
-			$(node).fadeOut(5000);
-		}
-	})
+img.src = "kartta.jpg";
+
+var Ylatitude = Math.round(Position.coords.latitude);
+var Xlongitude = Math.round(Position.coords.longitude);
+var Ycor = 180;
+var Xcor = 360;
+/*if (Ylatitude < 0){
+	Ycor = 180 - (Ylatitude * 2);
+}else{
+	Ycor = 180 + (Ylatitude * 2);
 }
-//triggering fade when user scrolls the page
-window.addEventListener("scroll", fadeOnView());
-//window.onscroll = $("#fadeTest").fadeToggle();
+	if (Xlongitude < 0){
+	Xcor = 360 - (Xlongitude * 2);
+}else{
+	Xcor = 360 + (Xlongitude * 2);
+}*/
+context.beginPath();
+context.moveTo(Xcor, Ycor);
+Context.lineTo(Xcor, Ycor + 5);
+context.stroke();
+}
+function DisplayError(Error) 
+{
+var message;
+switch(Error.code) {
+case 0: message = "Error retrieving location information";
+break;
+case 1: message = "User prevented location access";
+break;
+case 2: message = "Browser could not retrieve data";
+break;
+case 3: message = "Browser timed out during data retrieval";
+break;
+}
+alert(Message);
+}
