4ad4ba47d2be1c44d3105d809682d741c3cc3667
welitin14 <55680799+welitin14@users.noreply.github.com>
2019-10-30 20:03:17 +0200

    map pin added, shows visitors location

--- tuoli.js
+++ tuoli.js
@@ -1,56 +1,63 @@
 function ShowPosition()
 {
 if (navigator.geolocation)
-navigator.geolocation.getCurrentPosition(DisplayResult, DisplayError)
+	navigator.geolocation.getCurrentPosition(DisplayResult, DisplayError)
 else
-alert("Browser does not support geolocation");
+	alert("Browser does not support geolocation");
 }
 function DisplayResult(Position)
 {
-/*var message = " Latitude: " + Position.coords.latitude;
-message += " Longitude: " + Position.coords.longitude;
-message += " Accuracy: " + Position.coords.accuracy + " meters ";
-alert(message);*/
+	/*var message = " Latitude: " + Position.coords.latitude;
+	message += " Longitude: " + Position.coords.longitude;
+	message += " Accuracy: " + Position.coords.accuracy + " meters ";
+	alert(message);*/
 
-var context = document.getElementById('mapCanvas').getContext("2d");
-	
-var img = new Image();
-img.onload = function () {
+	var context = document.getElementById('mapCanvas').getContext("2d");	
+
+	var img = new Image();
+	img.onload = function () {
     context.drawImage(img, 0, 0);
-}
-img.src = "kartta.jpg";
+	}
+	img.src = "kartta.jpg";
+	
+	var img = new Image();
+	img.onload = function () {
+		context.drawImage(img, 0, 0);
+	}
+	img.src = "kartta.jpg";
 
-var Ylatitude = Math.round(Position.coords.latitude);
-var Xlongitude = Math.round(Position.coords.longitude);
-var Ycor = 180;
-var Xcor = 360;
-/*if (Ylatitude < 0){
-	Ycor = 180 - (Ylatitude * 2);
-}else{
-	Ycor = 180 + (Ylatitude * 2);
-}
+	var Ylatitude = Math.round(Position.coords.latitude);
+	var Xlongitude = Math.round(Position.coords.longitude);
+	var Ycor = 180;
+	var Xcor = 360;
+	if (Ylatitude < 0){
+		Ycor = 180 + (Ylatitude * 2);
+	}else{
+		Ycor = 180 - (Ylatitude * 2);
+	}
 	if (Xlongitude < 0){
-	Xcor = 360 - (Xlongitude * 2);
-}else{
-	Xcor = 360 + (Xlongitude * 2);
-}*/
-context.beginPath();
-context.moveTo(Xcor, Ycor);
-Context.lineTo(Xcor, Ycor + 5);
-context.stroke();
+		Xcor = 360 + (Xlongitude * 2);
+	}else{
+		Xcor = 360 - (Xlongitude * 2);
+	}
+	var img2 = new Image();
+	img2.onload = function () {
+		context.drawImage(img2, Xcor - 5, Ycor - 30);
+	}
+	img2.src = "mapPin.png";
 }
 function DisplayError(Error) 
 {
-var message;
-switch(Error.code) {
-case 0: message = "Error retrieving location information";
-break;
-case 1: message = "User prevented location access";
-break;
-case 2: message = "Browser could not retrieve data";
-break;
-case 3: message = "Browser timed out during data retrieval";
-break;
-}
-alert(Message);
+	var message;
+	switch(Error.code) {
+		case 0: message = "Error retrieving location information";
+		break;
+		case 1: message = "User prevented location access";
+		break;
+		case 2: message = "Browser could not retrieve data";
+		break;
+		case 3: message = "Browser timed out during data retrieval";
+		break;
+	}
+	alert(Message);
 }
